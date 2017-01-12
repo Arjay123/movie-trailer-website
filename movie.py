e3f0a1ea332b91445f5d8b07a0706c98f0e27ae3
Arjay Nguyen <Arjay.Nguyen@ul.com>
2017-01-12 13:16:13 -0800

    created movie class

---
+++ movie.py
@@ -0,0 +1,9 @@
+class Movie:
+	def __init__(self, title, trailer_youtube_url, poster_image_url):
+		self.title = title
+
+		#FIXME - check if valid url?
+		self.trailer_youtube_url = trailer_youtube_url
+		self.poster_image_url = poster_image_url
+
+
