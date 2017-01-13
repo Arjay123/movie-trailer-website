51cc94fb992bf9cb192d84249ad7db6e32273be2
arjaynguyen <arjaynguyen123@hotmail.com>
2017-01-12 17:48:18 -0800

    implemented get_movie_by_id

--- tmdb_api.py
+++ tmdb_api.py
@@ -3,9 +3,11 @@ import requests
 #get api key
 apifile = open('api_key.txt', 'r')
 api_key = apifile.read()
-
+api_key_query_str = "api_key=d993f52dbb1f8d190a966790f7590b3b"
 api_base = "https://api.themoviedb.org"
 search_base = api_base + "/3/search/movie?api_key=d993f52dbb1f8d190a966790f7590b3b&query="
+poster_base = "https://image.tmdb.org/t/p/original/"
+movie_base = "https://api.themoviedb.org/3/movie/"
 
 #search for movie by querying api w/ title, returns id if found, None if not
 def search_movie(query):
@@ -19,5 +21,28 @@ def search_movie(query):
 
 	return None
 
+#get movie info using id, returns dictionary w/ all movie information
+def get_movie_by_id(id):
+	query_url = movie_base + str(id) + "?" + api_key_query_str
+	data = requests.get(query_url)
 
-print(search_movie('oceans eleven'))
+	results = {}
+
+	if(data.status_code == 200):
+		json = data.json()
+
+		#get title
+		results['title'] = json['title']
+
+		#get image
+		#get trailer
+
+	return results
+
+def get_image_by_id(id):
+	query_url = movie_base + str(id) + "/images?" + api_key_query_str
+	data = requests.get(query_url)
+
+
+
+get_movie_by_id(161)
