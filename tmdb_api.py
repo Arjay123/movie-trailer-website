cc0cf2b700b1d58ecd4ae38f58242dc60f9721ec
arjaynguyen <arjaynguyen123@hotmail.com>
2017-01-12 18:39:57 -0800

    capitalized constant vars

--- tmdb_api.py
+++ tmdb_api.py
@@ -2,19 +2,19 @@ import requests
 
 #get api key
 apifile = open('api_key.txt', 'r')
-api_key = apifile.read()
-api_key_query_str = "api_key=d993f52dbb1f8d190a966790f7590b3b"
+API_KEY = apifile.read()
+API_KEY_QUERY_STR = "api_key=" + API_KEY
 
-api_base = "https://api.themoviedb.org"
-search_base = api_base + "/3/search/movie?" + api_key_query_str + "&query="
-movie_base = api_base + "/3/movie/"
+API_BASE = "https://api.themoviedb.org"
+SEARCH_BASE = API_BASE + "/3/search/movie?" + API_KEY_QUERY_STR + "&query="
+MOVIE_BASE = API_BASE + "/3/movie/"
 
-poster_base = "https://image.tmdb.org/t/p/original"
+POSTER_BASE = "https://image.tmdb.org/t/p/original"
 
 
 #search for movie by querying api w/ title, returns id if found, None if not
 def search_movie(query):
-	query_url = search_base + query
+	query_url = SEARCH_BASE + query
 	data = requests.get(query_url)
 
 	#check status code
@@ -25,7 +25,7 @@ def search_movie(query):
 
 #get movie info using id, returns dictionary w/ all movie information
 def get_movie_by_id(id):
-	query_url = movie_base + str(id) + "?" + api_key_query_str
+	query_url = MOVIE_BASE + str(id) + "?" + API_KEY_QUERY_STR
 	data = requests.get(query_url)
 
 	results = {}
@@ -46,11 +46,11 @@ def get_movie_by_id(id):
 
 #get movie poster by id, returns url of image if found
 def get_image_by_id(id):
-	query_url = movie_base + str(id) + "/images?" + api_key_query_str
+	query_url = MOVIE_BASE + str(id) + "/images?" + API_KEY_QUERY_STR
 	data = requests.get(query_url)
 
 	if data.status_code == 200 and len(data.json()['posters']) > 0:
-		return poster_base + data.json()['posters'][0]['file_path']
+		return POSTER_BASE + data.json()['posters'][0]['file_path']
 
 
 print(get_movie_by_id(161)['poster'])
