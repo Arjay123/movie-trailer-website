8107db266302574655e29228bf421c921cffca3a
Arjay Nguyen <Arjay.Nguyen@ul.com>
2017-01-13 11:51:48 -0800

    pep8 formatting and comments

--- media.py
+++ media.py
@@ -1,10 +1,17 @@
-class Movie:
-	def __init__(self, title, trailer_youtube_url, poster_image_url, overview):
-		self.title = title
+class Movie(object):
+    """
+    Represents a movie, containing information that a user may want to know
+    about the movie
 
-		#FIXME - check if valid url?
-		self.trailer_youtube_url = trailer_youtube_url
-		self.poster_image_url = poster_image_url
-		self.overview = overview
+    Attributes:
+            title - Title of the movie
+            trailer_youtube_url - Link to the youtube video of the movie's trailer
+            poster_image_url - Link to an image file of the movie's poster
+            overview - Quick synopsis of the movie's plot
+    """
 
-
+    def __init__(self, title, trailer_youtube_url, poster_image_url, overview):
+        self.title = title
+        self.trailer_youtube_url = trailer_youtube_url
+        self.poster_image_url = poster_image_url
+        self.overview = overview
